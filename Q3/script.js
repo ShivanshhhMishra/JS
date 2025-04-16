656dc0c84b152884655ff5c10bfc192bb23aa241
Shivanshh369 <shivanshhh369@gmail.com>
2025-04-16 19:55:34 +0530

    update

--- Q3/script.js
+++ Q3/script.js
@@ -1,7 +1,17 @@
 //jab bhi form ke saath deal karo yad rakho ki submit hone par forms page ko reload kar dete hai, aur tumhe khayaal rakhna hai ke pages reload naa ho, nahito agr page reload hua to js nahi chalegi,kyuki js chal paaye se phale he page reload ho chuka hoga
 
 var form = document.querySelector("form");
+var inps = document.querySelectorAll('input[type ="text"]');
+
+var h4 = document.querySelector("h4");
 
 form.addEventListener("submit", function (ev) {
   ev.preventDefault();
+  for (var i = 0; i < inps.length; i++) {
+    if (inps[i].value.trim() === "") {
+      h4.textContent = "Error,some fields are blank";
+      h4.style.color = "red";
+      break;
+    }
+  }
 });
